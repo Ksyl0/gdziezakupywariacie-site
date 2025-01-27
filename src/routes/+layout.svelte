22da7cdd0119d486fc8c3cf3a1d3cde78999442e
Ksyl <70941465+Ksyl0@users.noreply.github.com>
2025-01-27 23:06:55 +0100

    W.I.P

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,22 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="vh-100 container-flex d-flex vstack">
+	<div class="h-100">
+	  <div class="row h-100 g-0 justify-content-center">
+		<div class="col-lg-3 col-0 bg-light"></div>
+		<div
+		  class="col-lg-6 col-12 vstack d-flex align-items-center h-100 bg-light"
+		>
+		  {@render children()}
+		</div>
+		<div class="col-lg-3 col-0 bg-light"></div>
+	  </div>
+	</div>
+  </div>
+
+  <style>
+	* {
+		outline: 2px solid purple;
+	}
+  </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    let exampleCodeString: string = `GET "https://getfullyear.com/api/year"<br>
+                {<br>
+                    "year": 2025,<br>
+                    "year_string": "2025",<br>
+                    "sponsored_by": "Viagra: standing tall and proud"<br>
+                }<br>`
+</script>
+
+<div class="vh-100 v-stack" id="main">
+    <div class="text-base" id="intro">
+        <p class="mt-10 font-bold text-xl">
+            Czy użytkownik twojej aplikacji kiedykolwiek się ciebie zapytał:<br>
+            A w tym tygodniu jest handlowa czy niehandlowa?<br>
+            Jeśli twoja odpowiedź to TAK, to znajdujesz się we właściwym miejscu.
+        </p>
+        <br>Każdego dnia setki milionów polaków korzystając z internetu zadaje sobie pytanie<br>
+        "Cholera czy w tym tygodniu jest niedziela handlowa?"*
+        <br>Developerzy nie posiadający narzędzi do odpowiedzenia na to pytanie byli całkowicie bezradni, <bold class="font-bold">aż do teraz</bold>.
+        <p class="text-xs italic font-extralight">* Liczba ustalona na podstawie wróżb żula Mietka spod lidla</p>
+        <br>
+        Nasz zespół 2.5 inżynierów nieustannie pracował przez 5 dni żeby dostarczyć ostateczne API by rozwiązać problem niedzieli handlowych.
+    </div>
+    <div id="example" class="mt-12 text-base">
+        <p class="text-3xl f">
+            Banalnie proste API
+        </p>
+        <p id="exaple-code" class="border-gray-200">
+        </p>
+    </div>
+</div>
+
+<style>
+    * {
+        outline: 2px solid lavender
+    }
+</style>
