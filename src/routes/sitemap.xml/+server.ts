1fca58c6ff1c3db085dfafc0cee39b717533647e
Ksyl <70941465+Ksyl0@users.noreply.github.com>
2025-01-28 22:32:53 +0100

    Revert "feat: add sitemap"
    This reverts commit d08f4f3f6eff226f1e41f50611ac83bf6ccdb1aa.

--- src/routes/sitemap.xml/+server.ts
+++
@@ -1,8 +0,0 @@
-import * as sitemap from 'super-sitemap';
-import type { RequestHandler } from '@sveltejs/kit';
-
-export const GET: RequestHandler = async () => {
-  return await sitemap.response({
-    origin: 'https://gdziezakupywariacie.pl',
-  });
-};
