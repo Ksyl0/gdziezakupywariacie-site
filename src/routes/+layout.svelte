bb2a44a940b4c249fb304d556e41dedbafa9dbb3
Ksyl <70941465+Ksyl0@users.noreply.github.com>
2025-02-12 00:48:03 +0100

    refactor: get rid of bootstrap leftovers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,19 +3,7 @@
 	let { children } = $props();
 </script>
 
-<div class="vh-100 container-flex d-flex vstack">
-	<div class="h-100">
-	  <div class="row h-100 g-0 justify-content-center">
-		<div class="col-lg-3 col-0 bg-gray-100"></div>
-		<div
-		  class="col-lg-6 col-12 vstack d-flex align-items-center h-100"
-		>
-		  {@render children()}
-		</div>
-		<div class="col-lg-3 col-0 bg-gray-100"></div>
-	  </div>
-	</div>
-  </div>
+{@render children()}
 
   <style>
 	/* * {
