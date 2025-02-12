8c854ae0dc423eee773f29f3a3d20177ba988805
Ksyl <70941465+Ksyl0@users.noreply.github.com>
2025-02-12 23:30:14 +0100

    feat: add back sponsor message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     export let data;
     const { apiData, apiData1 } = data;
+    console.log(`Up to date footer was brought to you by:\n${apiData1.sponsored_by}`)
 </script>
 
 <svelte:head>
